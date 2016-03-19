d1e3ad59c22017996ae6a5d66a1bea4cda127cee
Mattdre <olsen_matthew@yahoo.com>
2016-03-18 20:47:03 -1000

    Japanese places

--- js/app.js
+++ js/app.js
@@ -42,7 +42,10 @@ Console log the variable
 Console log the value at index 1.
 Console log the value at index 3.*/
 
-
+var japanPrefectures = ['Shiga-ken','Nagasaki-ken','Fukui-ken','Chiba-ken','Fukuoka-ken'];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
 Console log the variable
