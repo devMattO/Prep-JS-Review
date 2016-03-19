b50f4bd6d80a4413f8a49205d1fa599072cefff8
Mattdre <olsen_matthew@yahoo.com>
2016-03-18 21:03:25 -1000

    hotel

--- js/app.js
+++ js/app.js
@@ -75,6 +75,10 @@ console.log(india);
 
  console log the object*/
 
+var hotel = {name:'Hilton',rooms:200,pool:true,checkIn: null};
+console.log(hotel);
+ 
+
 /*9.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
