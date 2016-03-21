67e6e691fb720237a4963a70222425f3b6f9e785
Mattdre <olsen_matthew@yahoo.com>
2016-03-21 02:30:37 -1000

    Antigua drinking vacation

--- js/app.js
+++ js/app.js
@@ -214,8 +214,25 @@ This function takes in a number parameter age and returns a Boolean value true o
 
 Invoke this function by passing in a number value.  Google search Antigua's minimum drinking age to confirm the number value.  Store the return value to a variable named drinkinAlready and console log this variable*/
 
+function oldEnough(age){
+  if (age >= 18){
+    return true;
+  }
+}
+var drinkinAlready = oldEnough(22);
+console.log(drinkinAlready);
+
 /*21. Declare a function named fightDaPower.
 This function checks the value stored at the drinkinAlready variable in the previous exercise and if the value is true, return the string "I'm jumping on the next flight to Antigua." Otherwise return the string "I can't wait to be in the 5th grade!"  Console log the result.*/ 
+
+function fightDaPower(areYouReady){
+  if (areYouReady === true){
+    return "I'm jumping on the next flight to Antigua.";
+  }else{
+    return "I can't wait to be in the 5th grade!";
+  }
+}
+console.log(fightDaPower(drinkinAlready));
 
 /*22. Declare a function named contentFiller which takes in a single parameter which will be an object.  Write a for loop that will put 5 random numbers between the values of 0 to 20 in the contents array in the bigBox object you created in exercise 10.  Console log the result.*/
 
